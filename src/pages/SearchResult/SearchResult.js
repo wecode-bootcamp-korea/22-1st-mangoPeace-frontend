a84c001e7166161ff55172efc3739f34d8a4c761
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-07-05 23:52:25 +0900

    상단 레이아웃 대락 완성

--- src/pages/SearchResult/SearchResult.js
+++ src/pages/SearchResult/SearchResult.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './SearchResult.scss';
 
 class SearchResult extends React.Component {
   render() {
-    return <div>SearchResult</div>;
+    return (
+      <>
+        <nav>SearchResult</nav>
+
+        <div className="searchResultList">
+          <div className="searchResultHead">
+            <div className="searchResultTitle">
+              <div className="searchKeyword">국밥</div>
+              <div className="resultRank"> 맛집 검색 순위</div>
+            </div>
+            <div className="searchResultFilter">
+              <i className="fas fa-list"></i>
+              <span className="letterFilter">필터</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
