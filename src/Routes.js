91401f37f0725bc217d9aa3ab46d05f6c5f9a073
kylee <anachrosh@gmail.com>
2021-07-05 18:19:31 +0900

    MODI: 초기 세팅 수정

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Nav from './components/Nav';
 import Detail from './pages/Detail/Detail';
-import BestList from './pages/Main/BestList';
+import BestList from './pages/BestList/BestList';
 import Main from './pages/Main/Main';
 import SearchResult from './pages/SearchResult/SearchResult';
 
@@ -14,9 +14,9 @@ class Routes extends React.Component {
         <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
-          <Route exact path="/SearchResult" component={SearchResult} />
-          <Route exact path="/BestList:id" component={BestList} />
-          <Route exact path="/Detail/:id" component={Detail} />
+          <Route exact path="/searchresult" component={SearchResult} />
+          <Route exact path="/bestlist:id" component={BestList} />
+          <Route exact path="/detail/:id" component={Detail} />
         </Switch>
       </Router>
     );
