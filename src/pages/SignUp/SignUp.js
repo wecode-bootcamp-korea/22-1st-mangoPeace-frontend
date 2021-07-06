d0c12f81b867bb293356a468e14a8874f8e6c102
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-07-06 16:32:30 +0900

    [Add]SignUp 모달 기본  레이아웃 작성중

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import './SignUp.scss';
 
 class SignUp extends React.Component {
   render() {
     return (
-      <div>SignUp</div>
-    )
+      <>
+        <form className="signUpForm">
+          <div className="signUpLogo">로고 이미지</div>
+          <div className="signUpTitle">회원가입</div>
+          <div className="signUpContainer">
+            <input className="inputName" placeholder="이름"></input>
+            <input className="inputEmail" placeholder="이메일"></input>
+            <input className="inputPassword" placeholder="비밀번호"></input>
+            <button className="submitInfoBtn">회원가입</button>
+          </div>
+          <div className="checkMembership">이미 가입 하셨나요 ? </div>
+        </form>
+      </>
+    );
   }
-};
+}
 
 export default SignUp;
