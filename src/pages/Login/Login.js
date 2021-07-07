347dc98657f35924a3f5eece80d1a2009f116a98
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-07-07 09:49:45 +0900

    [Add]Login 모달 레이아웃 스타일 적용 중

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -6,15 +6,20 @@ class Login extends React.Component {
     return (
       <>
         <form className="loginForm">
-          <div className="loginLogo">로고</div>
+          <div className="loginLogo">
+            <img className="logoImage" alt="logo" src="images/mango.png" />
+          </div>
           <div className="loginTitle">로그인</div>
           <input className="inputEmail"></input>
           <input className="inputPassword"></input>
+          <button className="submitLoginInfoBtn">로그인</button>
           <div className="askingMissingPassword">
             <div></div>
             비밀번호를 잊어버리셨나요?
           </div>
-          <div className="askingMembership">계정이 없으신가요? 회원가입</div>
+          <div className="askingMembership">
+            계정이 없으신가요? <a className="goToSignUp">회원가입</a>
+          </div>
         </form>
       </>
     );
