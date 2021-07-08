0b1e46f75ec2ce5e29b2633a0c6f2b59e98640e9
euiyeonlee <dmldus1121@naver.com>
2021-07-08 19:35:42 +0900

    MODI: Nav review feedback 1차 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,15 +6,15 @@ class Nav extends React.Component {
   render() {
     return (
       <div className="navBar">
-        <div className="navLeft">
+        <div className="logoBox">
           <h1>싸우지망고</h1>
           <img className="logoImg" src="images/mango.png" alt="로고이미지" />
         </div>
-        <div className="navCenter">
+        <div className="navSearchBar">
           <i class="fas fa-search" id="searchIcon"></i>
           <input className="mainSearchInput" placeholder="맛집 검색"></input>
         </div>
-        <div className="navRight">
+        <div className="navProfileBox">
           <button className="mainLoginBtn">로그인</button>
           <button className="mainSignUpBtn">회원가입</button>
           <img
