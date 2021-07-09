cbcf82d39d44f2a1ec48dafee3d13837ed7048b1
glorious-min <70811550+glorious-min@users.noreply.github.com>
2021-07-09 10:36:45 +0900

    [Add]필터레이아웃 구성 중

--- src/pages/Filter/Filter.js
+++ src/pages/Filter/Filter.js
@@ -6,41 +6,48 @@ class Filter extends React.Component {
     return (
       <>
         <div className="UpperBox">
-          <div className="searhFilter">
-            <div className="sectionHead">
+          <div className="searchFilter">
+            <div className="selectHead">
               <span className="Title">검색필터</span>
               <span className="multipleChoice">중복선택불가능</span>
             </div>
-            <span className="rating">평점순</span>
-            <span className="popularity">코멘트</span>
+            <div className="selectSection">
+              <div className="rating">평점순</div>
+              <div className="popularity">코멘트</div>
+            </div>
           </div>
           <div className="priceForOnePerson">
-            <div className="sectionHead">
-              <span className="Title">가격/1인당</span>
+            <div className="selectHead">
+              <span className="title">가격/1인당</span>
               <span className="multipleChoice">중복선택불가능</span>
             </div>
-            <span className="<10000">만원이하</span>
-            <span className="10000_">만원이상</span>
-            <span className="20000_">2만원이상</span>
-            <span className="30000_">3만원이상</span>
+            <div className="priceSelectSection">
+              <span className="lessThan10000">만원이하</span>
+              <span className="over10000">만원이상</span>
+              <span className="over20000">2만원이상</span>
+              <span className="over30000">3만원이상</span>
+            </div>
           </div>
 
           <div className="menuCategory">
-            <div className="sectionHead">
-              <span className="Title">음식종류</span>
+            <div className="selectHead">
+              <span className="title">음식종류</span>
               <span className="multipleChoice">중복 선택 불가능</span>
             </div>
             <div className="western">
+              <span className="selectWestern">양식</span>
               <span className="pizza">피자</span>
               <span className="pasta">파스타</span>
               <span className="hamburger">햄버거</span>
             </div>
             <div className="korean">
-              <span className="soup">스프</span>
-              <span className="barbeque">바베큐</span>
-              <span classsName="noodle">국수</span>
+              <span className="selectKorean">한식</span>
+              <span className="soup">국밥</span>
+              <span className="barbeque">고기</span>
+              <span className="noodle">국수</span>
             </div>
             <div className="japanese">
+              <span className="selectJapanese">일식</span>
               <span className="sushi">스시</span>
               <span className="cutlet">돈가스</span>
               <span className="donburi">덮밥</span>
