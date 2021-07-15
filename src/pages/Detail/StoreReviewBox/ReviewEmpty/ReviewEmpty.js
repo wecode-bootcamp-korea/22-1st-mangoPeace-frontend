40241f8d0a27b0c1a5973e77fc33b319109d0718
euiyeonlee <dmldus1121@naver.com>
2021-07-15 17:26:44 +0900

    i class->className

--- src/pages/Detail/StoreReviewBox/ReviewEmpty/ReviewEmpty.js
+++ src/pages/Detail/StoreReviewBox/ReviewEmpty/ReviewEmpty.js
@@ -7,7 +7,7 @@ class ReviewEmpty extends React.Component {
     return (
       <li className="reviewLi">
         <div className="reviewEmptyBox">
-          <i class="reviewEmptyIcon far fa-sad-tear"></i>
+          <i className="reviewEmptyIcon far fa-sad-tear"></i>
           <span className="reviewEmptyText">아직 등록된 리뷰가 없어요</span>
         </div>
       </li>
